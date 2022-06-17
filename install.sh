8db72fb2d72cc98860c88a90833cba83ad55e26d
timothy <timkibalama@gmail.com>
2022-06-17 11:16:30 +0300

    edited install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 apt update
 apt install python3
 apt install pip
-pip install pyftpdlib
-pip install opencv_camera
-apt install portaudio19-dev
-pip install pyAudio
 apt install git
 apt install ffmpeg libsm6 libxext6  -y
+apt install portaudio19-dev
+pip install pyftpdlib
+pip install opencv_camera
+pip install pyAudio
