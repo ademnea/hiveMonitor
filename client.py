775ad867e930441aa0514b85aa93126ccece28c3
timothy <timkibalama@gmail.com>
2022-06-17 17:50:26 +0300

    added access_server function to client.py

--- client.py
+++ client.py
@@ -4,37 +4,12 @@ import sqlite3
 import client_config
 
 
-def send_files():
-    if path.isfile(client_config.database_path):
-        conn = sqlite3.connect(client_config.database_path, detect_types=sqlite3.PARSE_COLNAMES)
-        conn.row_factory = sqlite3.Row
-
-        cursor = conn.cursor()
-        cursor.execute("select * from file where transferred=? limit ?",
-                       (0, client_config.TRANS_LIMIT))
-        rows = cursor.fetchall()
-
-        for row in rows:
-            row_dict = dict(row)
-            file = open(row_dict['file_path'], 'rb')
-            ftp.storbinary('STOR %s|>%s|%s'
-                           % (row_dict['file_name'], row_dict['file_type'], row_dict['hash_value']), file)
-            resp = ftp.getresp()
-            hash_index = resp.find('r=')
-            if hash_index > -1:
-                conn.execute("update file set transferred = ? where id = ?",
-                             (int(resp[hash_index + 2:]), row_dict['id']))
-                conn.commit()
-            file.close()
-
-
-if __name__ == "__main__":
+def access_server():
     try:
         ftp = FTP(client_config.server_address)
         print(ftp.welcome[4:])
         username = client_config.username
         password = client_config.password
-        logged_in = 0
         try:
             ftp.login(username, password)
             logged_in = 1
@@ -49,11 +24,48 @@ if __name__ == "__main__":
             print('sign up and log in successful.')
             logged_in = 1
         if logged_in == 1:
-            print('attempting sending files')
-            send_files()
-            print('Tasks finished, logging out')
+            return ftp
         else:
             print('Access denied!')
-        ftp.quit()
+            exit()
     except ConnectionRefusedError:
         print('Server down or can not be reached')
+
+
+def send_files():
+    if path.isfile(client_config.database_path):
+        conn = sqlite3.connect(client_config.database_path, detect_types=sqlite3.PARSE_COLNAMES)
+        conn.row_factory = sqlite3.Row
+
+        cursor = conn.cursor()
+        cursor.execute("select * from file where transferred=? limit ?",
+                       (0, client_config.TRANS_LIMIT))
+        rows = cursor.fetchall()
+
+        if len(rows) == 0:
+            print('...No files to send...')
+            conn.close()
+            return
+        ftp = access_server()
+        print('attempting sending files')
+
+        for row in rows:
+            row_dict = dict(row)
+            file = open(row_dict['file_path'], 'rb')
+            ftp.storbinary('STOR %s|>%s|%s'
+                           % (row_dict['file_name'], row_dict['file_type'], row_dict['hash_value']), file)
+            resp = ftp.getresp()
+            hash_index = resp.find('r=')
+            if hash_index > -1:
+                conn.execute("update file set transferred = ? where id = ?",
+                             (int(resp[hash_index + 2:]), row_dict['id']))
+                conn.commit()
+            file.close()
+        print('Tasks finished, logging out')
+        ftp.quit()
+    else:
+        print('No files to send')
+
+
+if __name__ == "__main__":
+    send_files()
