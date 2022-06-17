0c7dca990466b2f7766ea19ac7ddfdf5f2365cba
timothy <timkibalama@gmail.com>
2022-06-17 11:12:49 +0300

    edited install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+apt update
 apt install python3
 apt install pip
 pip install pyftpdlib
@@ -6,3 +7,4 @@ pip install opencv_camera
 apt install portaudio19-dev
 pip install pyAudio
 apt install git
+apt install ffmpeg libsm6 libxext6  -y
