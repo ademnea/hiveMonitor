105f90d255bd668e3a44bf7218129cd9258f1f94
timothy <timkibalama@gmail.com>
2022-06-17 16:22:17 +0300

    included function record_video to raspbian_capture.py

--- raspbian_capture.py
+++ raspbian_capture.py
@@ -9,24 +9,13 @@ class Capture:
         self.files = []
 
     def record_video(self, capture_duration=10):
-        pass
-        # vid_path = client_config.video_dir + 'vid' + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + '.mp4'
-        # out = cv2.VideoWriter(vid_path, fourcc, 20.0, (640, 480))
-        #
-        # start_time1 = time.time()
-        # while int(time.time() - start_time1) < capture_duration:
-        #     ret, frame = cap.read()
-        #     if ret:
-        #         frame = cv2.flip(frame, 90)
-        #         out.write(frame)
-        #         img_path = client_config.image_dir + 'img' + datetime.datetime.now().strftime(
-        #             "%Y-%m-%d_%H-%M-%S") + '.jpg'
-        #         cv2.imwrite(img_path, frame)
-        #         self.files.append([img_path, "image"])
-        #     else:
-        #         break
-        # self.files.append([vid_path, "video"])
-        # self.save_to_db()
+        camera = PiCamera()
+        vid_path = client_config.video_dir + 'vid' + uuid.uuid4().__str__() + '.h264'
+        camera.start_recording(vid_path)
+        camera.wait_recording(capture_duration)
+        camera.stop_recording()
+        self.files.append([vid_path, "video"])
+        self.save_to_db()
 
     def record_audio(self, record_seconds=10):
         pass
