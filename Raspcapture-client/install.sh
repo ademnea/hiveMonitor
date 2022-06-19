de4860bfbc6a4e513c3f2b534b55f2de0f77c888
timothy-mayweather <timkibalama@gmail.com>
2022-06-19 14:06:22 +0300

    removed Raspcapture-server and Raspcapture-client

--- Raspcapture-client/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-apt update
-apt install python3
-apt install pip
-apt install ffmpeg libsm6 libxext6  -y
-apt install portaudio19-dev
-pip install opencv_camera
-pip install pyAudio
--- Raspcapture-server/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-apt update
-apt install python3
-apt install pip
-pip install pyftpdlib
