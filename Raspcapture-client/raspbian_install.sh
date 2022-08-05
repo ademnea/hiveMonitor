03a209ff16a6267393abac705cfd981004e0015e
timothy-mayweather <timkibalama@gmail.com>
2022-08-05 14:13:38 +0300

    removed server

--- Raspcapture-client/raspbian_install.sh
+++ Raspcapture-client/raspbian_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 apt update
+pip install --upgrade pip setuptools wheel
 pip install picamera
-apt install libportaudio0 libportaudio2 libportaudiocpp0 portaudio19-dev
+apt install libportaudio0 portaudio19-dev python3-pyaudio libportaudio2 libportaudiocpp0
 pip install pyaudio
 apt install -y gpac
