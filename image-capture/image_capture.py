35072b04deb69e07624c48cb7f81477d5ab93b3b
Esther Kisakye <78255316+Kisakye-e@users.noreply.github.com>
2022-10-05 15:19:06 +0300

    Update image_capture.py
    Removed code unrelated to image capturing

--- image-capture/image_capture.py
+++ image-capture/image_capture.py
@@ -13,15 +13,7 @@ class Capture:
     def __init__(self):
         self.files = []
         self.camera = None
-
-    def change_format(self, file_path):
-        new_file_path = file_path
-        if file_path.endswith(".h264"):
-            new_file_path = file_path[:-5]+".mp4"
-            call("MP4Box -fps 30 -add "+file_path+" "+new_file_path, shell=True)
-            call("rm "+file_path, shell=True)
-        return new_file_path
-
+        
     def init_camera(self):
         if self.camera is None:
             self.camera = PiCamera()
