f53d2104ab67f76e018b80b9d49fe5bc43dfa6ff
Kisakye-e <eng.kisakye@gmail.com>
2022-11-03 07:11:41 +0300

    Added desired file naming

---
+++ transmitter/clientfk.py
@@ -0,0 +1,68 @@
+import paramiko
+from os import path
+import sqlite3
+import config
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect('137.63.185.94',username='hivemonitor', password= 'Ad@mnea321')
+print("Connected successfully")
+sftp = ssh.open_sftp()
+multimedia_urls = {'audio': config.audio_url,
+                   'image': config.image_url, 'video': config.video_url}
+
+# paths
+database_path = config.base_dir+"/multimedia/database.sqlite"
+
+def send_files():
+    if path.isfile(database_path):
+        conn = sqlite3.connect(
+            database_path, detect_types=sqlite3.PARSE_COLNAMES)
+        conn.row_factory = sqlite3.Row
+
+        cursor = conn.cursor()
+        cursor.execute("select * from file where transferred=? limit ?",
+                       (0, config.TRANS_LIMIT))
+        rows = cursor.fetchall()
+
+        if len(rows) == 0:
+            print('...No files to send...')
+            conn.close()
+            return
+
+        print('attempting sending files')
+
+        for row in rows:
+            row_dict = dict(row)
+            mult_url = multimedia_urls[row_dict['file_type']]
+            if mult_url == "":
+                print("url for "+row_dict['file_type'] +
+                      " is blank hence file not transferred.")
+                continue
+
+            file = open(row_dict['file_path'], 'rb')
+            #payload = {'title': row_dict['file_name'], "node_id": config.node_id,
+            #"longitude": config.longitude, "latitude": config.latitude}
+            
+            # files = [(row_dict['file_type'], (row_dict['file_type'],
+                    #   file, 'application/octet-stream'))]
+            ret= sftp.put(file,mult_url+row_dict['file_name'])            
+            file.close()            
+            if (ret):
+                conn.execute(
+                    "update file set transferred = 1 where id = "+str(row_dict['id']))
+                conn.commit()
+                print(row_dict['file_name']+" transferred successfully.")
+            else:
+                print(row_dict['file_name']+" transfer failed!")
+        print('closing.....')
+        sftp.close()
+        conn.close()
+        ssh.close()
+        exit()
+    else:
+        print('No files to send')
+
+
+if __name__ == "__main__":
+    send_files()
---
+++ transmitter/config.py
@@ -0,0 +1,18 @@
+# client configurations
+
+# This is an auto-generated file
+# You can edit these configurations directly or by running the setup.py script
+# Note that errors in the config file can result into the failure of the whole client
+# So be careful while editing, thanks 
+
+node_id = 1234
+username = "hiveMonitor"
+password = "Ad@mnea321"
+base_dir = "/home/pi/hiveMonitor/transmitter"
+TRANS_LIMIT = 28
+server_address = "http://ademnea.net:"
+image_url = "/var/www/html/ademnea_website/public/hiveimage/"
+audio_url = "/var/www/html/ademnea_website/public/hiveaudio/"
+video_url =  "/var/www/html/ademnea_website/public/hivevideo/"
+longitude = 10
+latitude = 10
